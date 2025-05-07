a63d38eda35544fb5cb767fbe0ccac36febcd1b0
Xico Trujano <xtrujano@mac.com>
2025-05-06 21:37:17 -0600

    UI Components Structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import store from './store';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import HomeView from './components/HomeView';
+import PostsList from './components/PostsList';
+import PostDetail from './components/PostDetail';
+import './styles/main.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div className="app">
+          <Header />
+          <div className="app-content">
+            <Sidebar />
+            <main className="main-content">
+              <Switch>
+                <Route exact path="/" component={HomeView} />
+                <Route path="/r/:subreddit" component={PostsList} />
+                <Route path="/post/:subreddit/:id" component={PostDetail} />
+                <Route path="/search" component={PostsList} />
+              </Switch>
+            </main>
+          </div>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
