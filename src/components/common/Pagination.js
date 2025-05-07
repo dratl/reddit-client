a1d88eca6140ed0e81116970800ed107f1070d8e
Xico Trujano <xtrujano@mac.com>
2025-05-06 21:57:26 -0600

    Common Components

---
+++ src/components/common/Pagination.js
@@ -0,0 +1,29 @@
+// src/components/common/Pagination.js
+import React from 'react';
+import './Pagination.css';
+
+const Pagination = ({ postsPerPage, totalPosts, currentPage, paginate }) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  if (pageNumbers.length <= 1) return null;
+
+  return (
+    <nav className="pagination">
+      <ul>
+        {pageNumbers.map(number => (
+          <li key={number} className={currentPage === number ? 'active' : ''}>
+            <button onClick={() => paginate(number)}>
+              {number}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
